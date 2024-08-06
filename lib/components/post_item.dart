b0e3f1f65639a20487ed9ba479f7e062c4bb2bb7
Yousef Mohammed <100593295+Youssef-Mohammad@users.noreply.github.com>
2024-08-06 20:33:16 +0300

    modified:   lib/components/post_item.dart

--- lib/components/post_item.dart
+++ lib/components/post_item.dart
@@ -13,22 +13,28 @@ class PostItem extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start, // Add this line
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment
+                .spaceBetween, // Align items on the left and right
             children: [
-              if (post.owner?.profilePicture != '')
-                Image.network(
-                  'https://cloud.appwrite.io/v1/storage/buckets/6683247c00056fdd9ceb/files/${post.owner?.profilePicture}/view?project=667d37b30023f69f7f74&mode=admin',
-                  width: 40,
-                  height: 40,
-                ),
-              SizedBox(
-                width: 12,
-              ),
-              Text(
-                '${post.owner?.name} ${post.owner?.lastName}',
-                style: AppText.subtitle3,
+              Row(
+                children: [
+                  if (post.owner?.profilePicture != '')
+                    Image.network(
+                      'https://cloud.appwrite.io/v1/storage/buckets/6683247c00056fdd9ceb/files/${post.owner?.profilePicture}/view?project=667d37b30023f69f7f74&mode=admin',
+                      width: 40,
+                      height: 40,
+                    ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Text(
+                    '${post.owner?.name} ${post.owner?.lastName}',
+                    style: AppText.subtitle3,
+                  ),
+                ],
               ),
               IconButton(
                 onPressed: () {},
@@ -40,7 +46,7 @@ class PostItem extends StatelessWidget {
             height: 12,
           ),
           Container(
-            alignment: AlignmentDirectional.topStart, // Add this line
+            alignment: AlignmentDirectional.topStart,
             child: Text(
               style: (post.image != null && post.image!.isNotEmpty)
                   ? AppText.subtitle2
@@ -64,7 +70,7 @@ class PostItem extends StatelessWidget {
             height: 1,
             thickness: 2,
             color: AppColors.primary,
-          )
+          ),
         ],
       ),
     );
